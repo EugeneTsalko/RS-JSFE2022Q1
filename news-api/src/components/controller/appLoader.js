bcb13abac100b17d1de731de6d312a9075795398
EugeneTsalko <tefworkmail@gmail.com>
2022-06-29 19:15:50 +0300

    fix: add apiKey

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ef100e99e34f4ba29c3d9b5fdc0fc5ef', // получите свой ключ https://newsapi.org/
         });
     }
 }
