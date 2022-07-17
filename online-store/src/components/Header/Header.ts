40ced0191e4071bbbcf00c43cc974a11477bcf9f
EugeneTsalko <tefworkmail@gmail.com>
2022-07-17 20:13:00 +0300

    feat: add some style in header

--- online-store/src/components/Header/Header.ts
+++ online-store/src/components/Header/Header.ts
@@ -1,11 +1,15 @@
 import '../Header/Header.scss';
 import { ROOT_HEADER } from '../../constants/root';
 
+
 export class Header {
   render(count: number) {
     const html = `
       <nav class="nav">
-        <a href="./index.html" class="nav__logo">Online store</a>
+        <a href="./index.html" class="nav__logo">
+          <img class="nav__logo-img" src="../../assets/img/guitar.svg" alt="logo">
+          <h1>Guitar Shop</h1>
+        </a>
         <ul class="nav__links">
           <li class="nav__links-item">Page 1</li>
           <li class="nav__links-item">Page 2</li>
@@ -13,7 +17,7 @@ export class Header {
         </ul>
         <button class="nav__cart-button">
           <img src="./assets/img/cart.svg" alt="Cart" class="nav__cart-img">
-          <div class="nav__cart-counter">${count}</div>
+          <div class="nav__cart-counter"><span>${count}</span></div>
         </button>
       </nav>
     `
